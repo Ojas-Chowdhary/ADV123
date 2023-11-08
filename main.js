4c7adf31442d8a3f8a7bbf633297b3d548c541f3
Ojas-Chowdhary <108580011+Ojas-Chowdhary@users.noreply.github.com>
2023-11-08 19:35:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+noseX=0;
+noseY=0;
+
+function preload() {
+}
+
+function setup() {
+    canvas = createCanvas(350, 350);
+    canvas.center();
+}
+
+function draw() {
+}
+
+function take_snapshot() {
+    save('My Name is John Cena.png')
+}
